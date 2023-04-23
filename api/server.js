fb997421663a0616918d4a298a68d38dd8c834e4
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2023-04-23 14:35:08 +0530

    Completed

--- api/server.js
+++ api/server.js
@@ -23,7 +23,7 @@ app.post('/todo/new',(req, res) => {
 app.delete('/todo/delete/:id',async(req,res)=>{
     const result =await Todo.findByIdAndDelete(req.params.id);
     res.json(result);})
-app.put('/todo/complete/:id',async(req,res)=>{
+app.get('/todo/complete/:id',async(req,res)=>{
     const todo=await Todo.findById(req.params.id);
     todo.complete=!todo.complete;
     todo.save();
